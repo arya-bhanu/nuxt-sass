4961593c012546bd6c4efeb213e7dedb239a09db
arya-bhanu <aryacreative.dev@gmail.com>
2024-04-16 08:51:46 +0700

    feat: oauth config

---
+++ pages/auth.vue
@@ -0,0 +1,35 @@
+<!-- App.vue -->
+<template>
+	<div class="w-full min-h-screen flex items-center">
+		<div class="max-w-[800px] w-full mx-auto">
+			<Card>
+				<CardTitle>
+					<p class="text-center py-4">Welcome</p>
+				</CardTitle>
+				<CardContent>
+					<Auth
+						:supabase-client="supabaseClient as any"
+						:appearance="{
+							theme: ThemeSupa,
+						}"
+						:providers="['google', 'github']"
+						:redirect-to="'/'"
+					/>
+				</CardContent>
+			</Card>
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+// Import predefined theme
+import { ThemeSupa } from '@supabase/auth-ui-shared';
+
+import { Auth } from '@nuxtbase/auth-ui-vue';
+
+const supabaseClient = useSupabaseClient();
+
+definePageMeta({
+	layout: 'home',
+});
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+	<div>
+		<h1>Home</h1>
+		{{ user }}
+	</div>
+</template>
+
+<script lang="ts" setup>
+const supaClient = useSupabaseClient();
+const user = supaClient.auth.getUser();
+</script>
+
+<style></style>
